858931aa954cddfb01eea070226584bb601ddfea
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-12 15:42:56 -0800

    Initialized database and connected to server

--- server/server.py
+++ server/server.py
@@ -3,29 +3,18 @@ import flask
 # import process
 from flask import Flask, request, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-
+from logging import Formatter, FileHandler
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-print app.config.get('CLIENT_ID')
+db = SQLAlchemy()
+db.create_all()
 
 
 @app.route('/')
 def home():
   return render_template('../client/index.html')
-
-
-# @app.route('/api/users/find/<id>')
-# def
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
   print 'Server has been initialized on port 5000'
