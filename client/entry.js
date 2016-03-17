0c79d5835839a7b0993dd3133d9f45521dec56ab
Gar Lee <gardarr@gmail.com>
2016-03-17 15:23:11 -0700

    Added descriptions for usage of store and middleware

--- client/entry.js
+++ client/entry.js
@@ -10,6 +10,17 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import promise from 'redux-promise';
 
+/** STORE 
+Store brings actions, action creators, and reducers all together. They hold the current application state, update 
+the state with dispatch, and registers listeners. 
+
+We use middleware "thunk" so that action creators can return
+functions, rather than actions. A "Thunk" basically allows functions to be created that can later be executed.
+Essentially, a thunk is a function that returns a function that. This allows for asynchronous functions to be 
+dispatched as if they were synchronous! 
+Refer here for more on Thunk: https://github.com/gaearon/redux-thunk
+**/
+
 const createStoreWithMiddleware = applyMiddleware(
   thunk,
   promise,
--- client/reducers/userdata.js
+++ client/reducers/userdata.js
@@ -1,5 +1,11 @@
 import * as actionTypes from '../actionTypes/actionTypes';
 
+/** REDUCERS ***
+Reducers change state based on the execution of an actionCreator. State is described in an object, which changes
+based on user interactions. Reducers should not mutate its' arguments or perform API calls (Those are done in actionCreators)
+Using Object.assign(), we are able to create a copy of state, and change the previous 'state' to the new 'spread' state (...state)
+**/
+
 const DEFAULT_STATE = [];
 
 const addUser = (state, action) => ([
