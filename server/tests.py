18d73e41b522f750381c9c9240d726b1b2455388
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-17 14:49:27 -0700

    Began creating back-end tests

---
+++ server/tests.py
@@ -0,0 +1,26 @@
+import unittest
+import models
+import controller
+import server
+
+class userCreation(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        app = 
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
