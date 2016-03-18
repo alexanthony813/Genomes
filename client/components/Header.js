c854b7f372887c052597294e53108d82169dcc1a
Gar Lee <gardarr@gmail.com>
2016-03-17 17:45:32 -0700

    Made the login button super fresh

--- client/components/Header.js
+++ client/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render () {
     return (
       <div className = 'header'>
-        <p>Hello som yoyoyo is going on</p>
+        <p>Muh name is GAR</p>
       </div>
       )
   }
