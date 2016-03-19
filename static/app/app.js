03f949805da24d56990eaa2ff399fc66014eee6f
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-19 12:31:22 -0700

    Fixed merge conflicts

--- static/app/app.js
+++ static/app/app.js
@@ -1,8 +1,16 @@
-angular.module('genome', ['genome.pool', 'genome.self', 'genome.relatives', 'ngRoute'])
+angular.module('genome', ['genome.pool', 'genome.self', 'genome.relatives', 'ngRoute', 'genome.d3Service'])
 .config(function($routeProvider){
   $routeProvider
   .when('/', {
     templateUrl: '/static/app/pool/pool.html',
-    controller: 'PoolController'
+    controller: 'PoolController',
+    authenticate : true
   })
+  .when('/self/', {
+    templateUrl : '/static/app/self/self.html',
+    controller : 'SelfController'
+  })
+  .otherwise({
+    redirectTo : 'www.google.com'
+  });
 });
