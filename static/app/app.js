c4b51a7f5add3955329d7bcc19058e80a4b2ad05
Gar Lee <gardarr@gmail.com>
2016-03-21 10:47:11 -0700

    Setup navbar component

--- static/app/app.js
+++ static/app/app.js
@@ -1,36 +1,35 @@
 var app = angular.module('genome', [
   'genome.pool', 
-  'genome.self', 
-  'genome.relatives', 
-  'genome.d3Service',
-  'genome.auth',
-  'ngRoute',
-  'ngCookies'
+  'genome.self',
+  'genome.relatives',
+  'genome.sideNav',
+  'ngRoute', 
+  'genome.d3Service'
 ])
 
-.config(function($routeProvider){
+.config(function ($routeProvider) {
   $routeProvider
-  .when('/signin', {
-    templateUrl: '/static/app/auth/signin.html',
-    controller: 'AuthController'
-  })
-  .when('/signout', {
-    templateUrl: '/static/app/auth/signout.html',
-    controller: 'AuthController'
-  })
-  .when('/pool', {
-    templateUrl: '/static/app/pool/pool.html',
-    controller: 'PoolController'
-  })
-  .when('/', {
-    templateUrl: '/static/app/pool/pool.html',
-    controller: 'PoolController'
-  })
-  .when('/self/', {
-    templateUrl : '/static/app/self/self.html',
-    controller : 'SelfController'
-  })
-  .otherwise({
-    redirectTo : '/'
-  });
+    .when('/signin', {
+      templateUrl: '/static/app/auth/signin.html',
+      controller: 'AuthController'
+    })
+    .when('/signout', {
+      templateUrl: '/static/app/auth/signout.html',
+      controller: 'AuthController'
+    })
+    .when('/pool', {
+      templateUrl: '/static/app/pool/pool.html',
+      controller: 'PoolController'
+    })
+    .when('/', {
+      templateUrl: '/static/app/pool/pool.html',
+      controller: 'PoolController'
+    })
+    .when('/self/', {
+      templateUrl : '/static/app/self/self.html',
+      controller : 'SelfController'
+    })
+    .otherwise({
+      redirectTo : '/'
+    });
 });
---
+++ static/app/sidenav/sideNav.js
@@ -0,0 +1,11 @@
+angular.module('genome.sideNav', [])
+
+.controller('sideNavController', function ($scope, $rootScope, $location, sideNavFactory) {
+	//Build out functions that manipulate page render based on option
+	//This should be in correspondence with just the pages rendered after landing
+	
+})
+
+.factory('sideNavFactory', function ($http) {
+	
+})
