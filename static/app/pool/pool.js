d0d6b4835421a14d65b2e7f8368e38bba48d1206
Peter Lollo <peteralollo@gmail.com>
2016-03-21 12:25:40 -0700

    Render d3 bubbles on pool page with associated relative data

--- static/app/pool/pool.js
+++ static/app/pool/pool.js
@@ -1,11 +1,64 @@
 angular.module('genome.pool', [])
-
-.controller('PoolController', function ($scope, Relatives, $rootScope) {
+.controller('PoolController', function($scope, d3Service, Relatives, $rootScope, $window) {
   $scope.relatives = [];
   $rootScope.rels = [];
-  $scope.myData = [10,20,30,40,60];
+  $scope.myData = [10,10,10,20];
+  $scope.circles = [];
+  var boardHeight = $window.innerHeight;
+  var boardWidth = $window.innerWidth;
+  console.log('height', boardHeight);
+  console.log('width', boardWidth);
 
+  //pop up message displaying relative data when user clicks on a bubble
+  var showRelative = function(bubble) {
+    alert('You share ' + bubble.relative.similarity + '% of your DNA with ' + bubble.relative.first_name + bubble.relative.last_name);
+  }
 
+  //Grab the pool as a canvas for our bubbles
+  var svg = d3.select('.pool').append('svg')
+              .attr('width', boardWidth)
+              .attr('height', boardHeight);
+
+  //Create bubbles
+  var createBubbles = function(circleData) {
+    bubbles = svg.selectAll('circle')
+                 .data(circleData)
+                 .enter()
+                 .append('circle')
+                 .on('click', function(bubble) { showRelative(bubble);})
+                 .attr('class', 'enter')
+                 .attr('cx', function(d){return d.cx + Math.random() * 1000;})
+                 .attr('cy', function(d) {return d.cy + Math.random() * 1000;})
+                 .attr('r', function(d) {return d.r;})
+                 .style('fill', function() {
+                   return 'rgb(' + parseInt(Math.random() * 255) + ','
+                     + parseInt(Math.random() * 255) + ','
+                     + parseInt(Math.random() * 255) + ')';
+                 })
+                 .transition().duration(1500)
+                 .attr('cx', function(d) {
+                   return d.cx;
+                 }).attr('cy', function(d) {
+                   return d.cy;
+                 }).attr('r', function(d) {
+                   return d.r;
+                 });
+  };
+
+  //After grabbing relatives from the DB, create a bubbles array based on length of relatives array
+  var initialize = function(){
+    for (var i = 0; i <= $scope.relatives.length; i++) {
+      $scope.circles.push({
+        cx: Math.random() * boardWidth,
+        cy: Math.random() * boardHeight,
+        r: Math.random() * 100,
+        relative: $scope.relatives[i]
+      });
+    }
+    createBubbles($scope.circles);
+  };
+
+ //Grab relatives from the database, then initialize bubbles
   $scope.getRelatives = function (){
     Relatives.getRelatives()
     //Can refactor to return the promise values within the relatives factory if so desired
@@ -14,50 +67,9 @@ angular.module('genome.pool', [])
       $scope.relatives = relatives.data.relativeList;
       //Add relatives to rootScope to allow access within other controllers
       $rootScope.rels = relatives.data.relativeList;
+      initialize();
     }, function(err) {
       console.error('Error retrieving relatives: ', err)
     })
   }
-})
-//example of a working barsChart directive
-.directive('barsChart', function (d3Service, $parse) {
-     //explicitly creating a directive definition variable
-     //this may look verbose but is good for clarification purposes
-     //in real life you'd want to simply return the object {...}
-     var directiveDefinitionObject = {
-         //We restrict its use to an element
-         //as usually  <bars-chart> is semantically
-         //more understandable
-         restrict: 'E',
-         //this is important,
-         //we don't want to overwrite our directive declaration
-         //in the HTML mark-up
-         replace: false,
-         //our data source would be an array
-         //passed thru chart-data attribute
-         scope: {data: '=chartData'},
-         link: function (scope, element, attrs) {
-           //in D3, any selection[0] contains the group
-           //selection[0][0] is the DOM node
-           //but we won't need that this time
-           var chart = d3.select(element[0]);
-           //to our original directive markup bars-chart
-           //we add a div with out chart stling and bind each
-           //data entry to the chart
-            chart.append("div").attr("class", "chart")
-             .selectAll('div')
-             .data(scope.data).enter().append("div")
-             .transition().ease("elastic")
-             .style("width", function(d) { return d + "%"; })
-             .text(function(d) { return d + "%"; });
-           //a little of magic: setting it's width based
-           //on the data value (d)
-           //and text all with a smooth transition
-         }
-      };
-      return directiveDefinitionObject;
-   });
-
-function Ctrl($scope) {
-    $scope.myData = [10,20,30,40,60];
-};
+});
