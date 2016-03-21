7c11d85bfc74c9d7e93a702e33cd0c9616e85c9e
Gar Lee <gardarr@gmail.com>
2016-03-21 10:28:42 -0700

    Built out html containers for pool and self

--- static/app/self/self.js
+++ static/app/self/self.js
@@ -1,4 +1,10 @@
 angular.module('genome.self', [])
-.controller('SelfController', function(){
+
+.controller('SelfController', function ($scope, $rootScope, $location) {
+  
   $scope.stuff = [1,2,3,4];
+})
+
+.factory('selfFactory', function ($http) {
+	
 });
