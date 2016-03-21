bcdaaa5a24fbc29af9fa1c780995db9856149520
Gar Lee <gardarr@gmail.com>
2016-03-21 10:36:56 -0700

    Flushed out blueprint for self controller/factory

--- static/app/self/self.js
+++ static/app/self/self.js
@@ -1,10 +1,32 @@
 angular.module('genome.self', [])
 
-.controller('SelfController', function ($scope, $rootScope, $location) {
+.controller('SelfController', function ($scope, $rootScope, $location, SelfFactory) {
+ 	//Build out retreival functions to send out request on initialization for SNPs
+ 	//$scope.snps will hold each snp as the key, and a string of detailed information regarding each snp
+ 	$scope.snps = {};
+
   
-  $scope.stuff = [1,2,3,4];
 })
 
-.factory('selfFactory', function ($http) {
-	
+.factory('SelfFactory', function ($http) {
+
+/** 
+	* Used to retrieve information about SNPs pertaining to currently logged in user
+**/
+	var getSnps = function (userId) {
+		return $http({
+			method: 'POST',
+			url: '/api/snpsdata'
+			data: userId
+		}).then(function (snps) {
+			//do something with the snps retrieved here
+		}).catch(function (err) {
+			console.error('An error occured retreiving your SNPs ', err);
+		});
+	};
+
+	return {
+		getSnps: getSnps
+	}
+
 });
