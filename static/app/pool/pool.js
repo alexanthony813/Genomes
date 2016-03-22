9e7ae513ebe0e49b38933677ab3dd7543749f29d
Peter Lollo <peteralollo@gmail.com>
2016-03-21 19:07:24 -0700

    Clean up code on pool js

--- static/app/pool/pool.js
+++ static/app/pool/pool.js
@@ -10,22 +10,20 @@ angular.module('genome.pool', [])
     name: '',
     similarity: '',
     image: '../../assets/hipDNA.png'
-};
-/////FORCE TEST
+  };
 
-var margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
-},
-width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  var margin = {
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50
+  }
+  var width = 500 - margin.left - margin.right
+  var height = 500 - margin.top - margin.bottom;
 
-var padding = 2;
-var radius = d3.scale.sqrt().range([0, 12]);
+  var padding = 2;
+  var radius = d3.scale.sqrt().range([0, 12]);
 
-/////END FORCE TEST
   //pop up message displaying relative data when user clicks on a bubble
   var showRelative = function(bubble) {
     $scope.popModal.name = bubble.relative.first_name + ' ' + bubble.relative.last_name;
@@ -42,9 +40,8 @@ var radius = d3.scale.sqrt().range([0, 12]);
 
   //Create bubbles
   var createBubbles = function(circleData) {
-
     nodes = $scope.circles
-
+    //Add d3 force effect to layout
     var force = d3.layout.force()
         .nodes(nodes)
         .size([width, height])
@@ -52,111 +49,79 @@ var radius = d3.scale.sqrt().range([0, 12]);
         .charge(0)
         .on("tick", tick)
         .start();
+    //Add bubbles to DOM
+    var circle = svg.selectAll("circle")
+      .data(nodes)
+      .enter().append("circle")
+      .on('mouseover', function(){
+        d3.select(this).transition().attr('r', function(d) {return d.radius * 2;})
+      })
+      .on('mouseout', function(){
+        d3.select(this).transition().attr('r', function(d){return d.radius})
+      })
+      .on('click', function(bubble) {
+        $scope.$apply(
+        showRelative(bubble)
+        );
+      })
+      .attr("data-target", "#myModal")
+      .attr("data-toggle", "modal")
+      .attr("r", function (d) {
+        return d.radius;
+      })
+      .style('fill', function(d) {
+         return d.color;
+       })
+      .call(force.drag);
 
-        var circle = svg.selectAll("circle")
-          .data(nodes)
-          .enter().append("circle")
-          .on('mouseover', function(){
-            d3.select(this).transition().attr('r', function(d) {return d.radius * 2;})
-          })
-          .on('mouseout', function(){
-            d3.select(this).transition().attr('r', function(d){return d.radius})
-          })
-          .on('click', function(bubble) {
-            $scope.$apply(
-            showRelative(bubble)
-            );
-          })
-          .attr("data-target", "#myModal")
-          .attr("data-toggle", "modal")
-          .attr("r", function (d) {
-            return d.radius;
-          })
-          .style('fill', function(d) {
-             return d.color;
-           })
-          .call(force.drag);
+    //Control bubble entry onto DOM and magnetic resistance to each other
+    function tick(e) {
+      circle.each(gravity(0.8 * e.alpha))
+      .each(collide(0.5))
+      .attr("cx", function (d) {
+        return d.x;
+      })
+      .attr("cy", function (d) {
+        return d.y;
+      });
+    }
 
+    // Move nodes toward cluster focus.
+    function gravity(alpha) {
+      return function (d) {
+          d.y += (d.cy - d.y) * alpha;
+          d.x += (d.cx - d.x) * alpha;
+      };
+    }
 
-          function tick(e) {
-              circle.each(gravity(0.8 * e.alpha))
-              .each(collide(0.5))
-              .attr("cx", function (d) {
-                return d.x;
-              })
-              .attr("cy", function (d) {
-                return d.y;
-              });
+    // Resolve collisions between nodes.
+    function collide(alpha) {
+      var quadtree = d3.geom.quadtree(nodes);
+      return function (d) {
+        var r = d.radius + radius.domain()[1] + padding,
+            nx1 = d.x - r,
+            nx2 = d.x + r,
+            ny1 = d.y - r,
+            ny2 = d.y + r;
+        quadtree.visit(function (quad, x1, y1, x2, y2) {
+            if (quad.point && (quad.point !== d)) {
+                var x = d.x - quad.point.x,
+                    y = d.y - quad.point.y,
+                    l = Math.sqrt(x * x + y * y),
+                    r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
+                if (l < r) {
+                    l = (l - r) / l * alpha;
+                    d.x -= x *= l;
+                    d.y -= y *= l;
+                    quad.point.x += x;
+                    quad.point.y += y;
+                }
             }
-
-          // Move nodes toward cluster focus.
-          function gravity(alpha) {
-              return function (d) {
-                  d.y += (d.cy - d.y) * alpha;
-                  d.x += (d.cx - d.x) * alpha;
-              };
-          }
-
-            // Resolve collisions between nodes.
-            function collide(alpha) {
-                var quadtree = d3.geom.quadtree(nodes);
-                return function (d) {
-                    var r = d.radius + radius.domain()[1] + padding,
-                        nx1 = d.x - r,
-                        nx2 = d.x + r,
-                        ny1 = d.y - r,
-                        ny2 = d.y + r;
-                    quadtree.visit(function (quad, x1, y1, x2, y2) {
-                        if (quad.point && (quad.point !== d)) {
-                            var x = d.x - quad.point.x,
-                                y = d.y - quad.point.y,
-                                l = Math.sqrt(x * x + y * y),
-                                r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
-                            if (l < r) {
-                                l = (l - r) / l * alpha;
-                                d.x -= x *= l;
-                                d.y -= y *= l;
-                                quad.point.x += x;
-                                quad.point.y += y;
-                            }
-                        }
-                        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-                    });
-                };
-            }
-
-
-
-/////Good below
-
-    // bubbles = svg.selectAll('circle')
-    //              .data(circleData)
-    //              .enter()
-    //              .append('circle')
-    //              .on('mouseover', function(){
-    //               d3.select(this).transition().attr('r', function(d) {return d.r * 2;})
-    //              })
-    //              .on('mouseout', function(){
-    //               d3.select(this).transition().attr('r', function(d){return d.r})
-    //              })
-    //              .on('click', function(bubble) { showRelative(bubble);})
-    //              .attr('class', 'enter')
-    //              .attr('cx', function(d){return d.cx + Math.random() * 1000;})
-    //              .attr('cy', function(d) {return d.cy + Math.random() * 1000;})
-    //              .attr('r', function(d) {return d.r;})
-    //              .style('fill', function() {
-    //                return 'rgb(' + parseInt(Math.random() * 255) + ','
-    //                  + parseInt(Math.random() * 255) + ','
-    //                  + parseInt(Math.random() * 255) + ')';
-    //              })
-    //              .transition().duration(1500)
-    //              .attr('cx', function(d) {
-    //                return d.cx;
-    //              }).attr('cy', function(d) {
-    //                return d.cy;
-    //              }).attr('r', function(d) {
-    //                return d.r;
-    //              })
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        });
+      };
+    }
+  //close createBubbles function
   };
 
   //After grabbing relatives from the DB, create a bubbles array based on length of relatives array
@@ -189,7 +154,7 @@ var radius = d3.scale.sqrt().range([0, 12]);
       console.error('Error retrieving relatives: ', err)
     })
   }
-
+  //Initialize the page with a call to getRelatives
   $scope.getRelatives();
 });
 
