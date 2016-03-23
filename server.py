8983ddaa3e3e96c26cdde4fbd39d7a7e0dc98ffd
Peter Lollo <peteralollo@gmail.com>
2016-03-22 17:12:54 -0700

    Remove print access token command

--- server.py
+++ server.py
@@ -74,8 +74,15 @@ def getRelatives():
         #Grab only relatives who match the relatives in the user_relatives_ids storage
         if relative.serialize()['id'] in user_relatives_ids:
             finalRelatives.append(relative.serialize())
+<<<<<<< bce313ea8273ed41f26bf12a8694228124951fbb
 
     return jsonify({'relativeList': finalRelatives})
+=======
+    print 'FINAL RELATIVES==========>>>>>>>>>>>>>>>>', finalRelatives
+    return jsonify({'relativeList' : finalRelatives})
+
+
+>>>>>>> Remove print access token command
 
 @app.route('/api/getsnps', methods=['POST', 'GET'])
 def getSnps():
@@ -117,7 +124,6 @@ def receive_code():
     #get access token from 23andMe
     if response.status_code == 200:
         access_token = response.json()['access_token']
-        print 'ACCESS_TOKEN =====================>>>>>>>>>>>>>>>>>', access_token
         headers = {'Authorization': 'Bearer %s' % access_token}
 
         #Begin API calls to 23andMe to get all scoped user data
--- static/app/self/self.js
+++ static/app/self/self.js
@@ -1,10 +1,8 @@
 angular.module('genome.self', [])
-
 .controller('SelfController', function ($scope, $rootScope, $cookies, $location, SelfFactory, d3Service) {
-  
+
   $scope.outcomelist = $rootScope.outcomes;
-
-
+  console.log('outcomes list: ', $scope.outcomelist);
     /* The 'FILLS' block will determine the availability of colors, balls and lines
      * and what quantity and other attributes the d3 plot should contain */
   var fills = ['#E74C3C', '#3498DB', '#2ECC71'],
@@ -17,68 +15,21 @@ angular.module('genome.self', [])
     x = d3.scale.linear().range([20, w - 20]),
     y = d3.scale.linear().range([h, 20]),
     z = d3.scale.linear().range([20, 5]);
-
-  /** 
-    * This block will append the built svg elements to the "body" of the HTML
-  **/
-  var svg = d3.select(".dnahelixcontainer")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h)
-  /** Appends the inner SVG to a larger SVG container **/
-  
-  svg.append("DNAHelix")
-      .attr("width", w)
-      .attr("height", h)
-      .attr("fill", "white")
-
-
-    var container = svg.append("g");
-
-    var counter = 0;
-
-    function generateData() {
-      counter++;
-      var data = d3.range(numX).map(function (d) {
-          var t = d * torsion - speed * counter;
-          return [{ x: Math.cos(t),
-                    y: d,
-                    z: Math.sin(t)
-                  },
-                  { x: Math.cos(t - Math.PI),
-                    y: d,
-                    z: Math.sin(t - Math.PI)
-                  }]
-      });
-      var flat = _.flatten(data);
-      x.domain(d3.extent(flat, function(d){ return d.x }));
-      y.domain(d3.extent(flat, function(d){ return d.y }));
-      z.domain(d3.extent(flat, function(d){ return d.z }));
-      return data
-    };
-
-    function draw () {
-      var cont = container.selectAll("g").data(generateData());
-      cont.exit().remove();
-      /** 
-         * The following D3 Element will create an instance of a new circle
-        * We want each circle to be unique, and correspond with a particular SNP
+      /**
+        * This block will append the built svg elements to the "body" of the HTML
       **/
   var svg = d3.select(".dnahelixcontainer")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
   /** Appends the inner SVG to a larger SVG container **/
-  
+
   svg.append("DNAHelix")
       .attr("width", w)
       .attr("height", h)
       .attr("fill", "white");
-
   var container = svg.append("g");
-
   var counter = 0;
-
   function generateData() {
     counter++;
     var data = d3.range(numX).map(function (d) {
@@ -98,7 +49,6 @@ angular.module('genome.self', [])
       z.domain(d3.extent(flat, function(d){ return d.z; }));
       return data;
   }
-
   function draw () {
     var cont = container.selectAll("g").data(generateData());
     cont.exit().remove();
@@ -111,16 +61,16 @@ angular.module('genome.self', [])
             .enter()
             .append("circle")
             .attr("fill", "black")
-            .on("click", function () { 
+            .on("click", function () {
                 console.log('clicking on a ball!!!');
               });
           d3.select(this).append('line')
-              .attr("stroke", function (d, i) { 
-                return fills[index%3]; 
+              .attr("stroke", function (d, i) {
+                return fills[index%3];
               })
               .attr("stroke-width", 2);
           });
-          
+
         cont.each(function (d, index) {
           var inverted = (d[0].y < d[1].x) ? 1 : -1;
           d3.select(this)
@@ -130,7 +80,7 @@ angular.module('genome.self', [])
             .attr("cy", function (d) { return y(d.y); })
             .attr("r",  function (d) { return z(d.z); })
             .attr("fill-opacity", function (d) { return z(d.z) / 10;})
-            .attr("fill", function (d, i) { return fills[index % 3]; })
+            .attr("fill", function (d, i) { return fills[index%3]; })
             .on("mouseover", function (d, i) {
               d3.select(this).transition()
               .attr('fill', function (d) {
@@ -146,72 +96,27 @@ angular.module('genome.self', [])
               .attr("x2", x(d[0].x) + inverted * z(d[0].z))
               .attr("x1", x(d[1].x) - inverted * z(d[1].z))
               .attr("y2", y(d[0].y))
-              .attr("y1", y(d[0].y));               
+              .attr("y1", y(d[0].y));
           });
       }
-
-      var inverted = (d[0].y < d[1].x) ? 1 : -1;
-
-      /** 
-        * The blocks below are responsible for filling in the coloring of each ball and line
-      **/
-      d3.select(this)
-          .selectAll("circle")
-          .data(d)
-          .attr("cx", function (d) { return x(d.x) })
-          .attr("cy", function (d) { return y(d.y) })
-          .attr("r",  function (d) { return z(d.z) })
-          .attr("fill-opacity", function (d) { return z(d.z) / 10 })
-          .attr("fill", function (d, i) { return fills[index%3]; })
-          .on("mouseover", function (d, i) {
-              d3.select(this).transition()
-              .attr('fill', function (d) {
-                console.log('d: ', d, 'i: ', i);
-
-              })
-              .attr('fill-opacity', function (d) {
-                return z(d.z) / 1
-              })
-              .attr('stroke-opacity', 0.5);
-            })
-          
-      /** EACH circle will have this info --- **/ 
-      /** Give Each circle their own values for circle fill="#ETC" 
-      We can create functions that determine what color it is, and then ensure the color pattern
-      matches a certain SNP  
-      **/
-
-      d3.select(this)
-          .select('line')
-          .attr("x2", x(d[0].x) + inverted * z(d[0].z))
-          .attr("x1", x(d[1].x) - inverted * z(d[1].z))
-          .attr("y2", y(d[0].y))
-          .attr("y1", y(d[0].y))                
-        })
-    };
-
-  setInterval(draw, 25);
+    setInterval(draw, 25);
 })
-
 .factory('SelfFactory', function ($http) {
-
-  /** 
-    * Used to retrieve information about SNPs pertaining to currently logged in user
-  **/
+/**
+  * Used to retrieve information about SNPs pertaining to currently logged in user
+**/
   var getSnps = function (userId) {
     return $http({
       method: 'POST',
       url: '/api/getsnps',
       data: userId
     }).then(function (snps) {
-      return snps.data;
+      return snps.data.outcomes;
     }).catch(function (err) {
       console.error('An error occured retreiving your SNPs ', err);
     });
   };
-
   return {
     getSnps: getSnps
   };
-
 });
