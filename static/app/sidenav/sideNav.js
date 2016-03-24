7431132ca77ca3b417d4df7d1d3fdd73f610682b
Peter Lollo <peteralollo@gmail.com>
2016-03-24 14:48:13 -0700

    Add side nav icons

--- static/app/sidenav/sideNav.js
+++ static/app/sidenav/sideNav.js
@@ -5,6 +5,18 @@ angular.module('genome.sideNav', [])
     controller: function ($scope, $rootScope, $location, SelfFactory) {
       $scope.expand = false;
       $scope.target;
+      $scope.view;
+      view = {
+        '/' : 'pool',
+        '/pool': 'pool',
+        'self': 'self'
+      }
+
+      var whichView = function() {
+        $scope.view = view[$location.$$path]
+      };
+      console.log('scope.view', $location);
+      whichView();
 
       $scope.toggleNavList = function (ev) {
         if (!(ev.type === 'mouseleave' && $scope.lastEventType === 'click')) {
