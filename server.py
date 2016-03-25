591204425bd4e72d6fc58029326ced8ccb41b581
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-25 11:47:44 -0700

    Began basic testing on back end

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import jwt
 from encoder import jwt_encode
 from logging import Formatter, FileHandler
-import models
 import controller
 from os import path
 import models
@@ -55,6 +54,7 @@ def getUser():
 
 @app.route('/demo/')
 def makeDemoUser():
+    print 'makeDemoUser is being called yo'
     #Add demo user to DB if they don't already exist
     controller.create_demo_user()
     demo_profile_id = 'demo_id'
