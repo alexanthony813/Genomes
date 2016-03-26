efe6f9fd12a356956bc9302969913f916aa5322f
Peter Lollo <peteralollo@gmail.com>
2016-03-26 16:35:18 -0700

    added os to models file

--- models.py
+++ models.py
@@ -7,6 +7,22 @@ from psycopg2 import connect
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from server import app
 
+
+import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 #Initialize postgreSQL genome database
 engine = create_engine('postgres://ekmsxuepfrzsrq:eheMsYxYxlKSuLbKvgyrxkAWlH@ec2-54-225-102-131.compute-1.amazonaws.com:5432/deued1oqupgabe', convert_unicode=True)
 #engine = create_engine('postgres://localhost/genome', convert_unicode=True)
