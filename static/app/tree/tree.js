d27297490035619128b24b0fd9abce9a694bc7ac
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-28 22:48:40 -0700

    Added new flatten function to orient the tree differently. Modifications made to recursive createTree function, still needs work

--- static/app/tree/tree.js
+++ static/app/tree/tree.js
@@ -99,11 +99,24 @@ angular.module('genome.tree', [])
     }
 
     function recursiveAdd(parentNode, newNodes){
-      if(parentNode.children.length === 0){
-        newNodes.forEach(function(node){
-          parentNode.children.push(node);
-        });
+      if(parentNode.children.length < 2 && newNodes.length > 0){
+        console.log(newNodes.length)
+
+        var first = newNodes.pop();
+        console.log(newNodes.length)
+        parentNode.children.push(first);
+        if(newNodes.length > 0){
+          console.log('second?')
+          var second = newNodes.pop();
+          parentNode.children.push(second);
+        }
+        if(newNodes.length > 0){
+          recursiveAdd(parentNode, newNodes);
+        }
       } else {
+        if(newNodes.length >1 && parent.children && parent.children[1]){
+          recursiveAdd(parentNode.children[1], newNodes.pop());
+        }
         recursiveAdd(parentNode.children[0], newNodes);
       }
     }
@@ -188,7 +201,7 @@ angular.module('genome.tree', [])
 
     //Add d3 force effect to layout
     var force = d3.layout.force()
-      .linkDistance(80)
+      .linkDistance(30)
       .charge(-120)
       .gravity(0.0)
       .size([width, height])
@@ -363,58 +376,58 @@ angular.module('genome.tree', [])
     //   return nodes;
     // }
 
-    function flatten(root) {
-  var nodes = [], i = 0, depth = 0, level_widths = [1], max_width, max_depth = 1, kx, ky;
+  function flatten(root) {
+    var nodes = [], i = 0, depth = 0, level_widths = [1], max_width, max_depth = 1, kx, ky;
 
-  function recurse(node, parent, depth, x) {
-    if (node.children) {
-      var w = level_widths[depth + 1] || 0;
-      level_widths[depth + 1] = w + node.children.length;
-      max_depth = Math.max(max_depth, depth + 1);
-      node.size = node.children.reduce(function(p, v, i) {
-        return p + recurse(v, node, depth + 1, w + i);
-      }, 0);
+    function recurse(node, parent, depth, x) {
+      if (node.children) {
+        var w = level_widths[depth + 1] || 0;
+        level_widths[depth + 1] = w + node.children.length;
+        max_depth = Math.max(max_depth, depth + 1);
+        node.size = node.children.reduce(function(p, v, i) {
+          return p + recurse(v, node, depth + 1, w + i);
+        }, 0);
+      }
+      if (!node.id) node.id = ++i;
+      node.parent = parent;
+      node.depth = depth;
+      //node.fixed = 8;
+      if (!node.px && !node.fixed && 0) {
+        node.y = depth;
+        node.x = x;
+      }
+      nodes.push(node);
+      return node.size;
     }
-    if (!node.id) node.id = ++i;
-    node.parent = parent;
-    node.depth = depth;
-    //node.fixed = 8;
-    if (!node.px && !node.fixed && 0) {
-      node.y = depth;
-      node.x = x;
-    }
-    nodes.push(node);
-    return node.size;
-  }
 
-  root.size = recurse(root, null, 0, 0);
+    root.size = recurse(root, null, 0, 0);
 
-  if (0) {
-    // now correct/balance the x positions:
-    max_width = 1;
-    for (i = level_widths.length; --i > 0; ) {
-      max_width = Math.max(max_width, level_widths[i]);
-    }
-    kx = (width - 20) / max_width;
-    ky = (height - 20) / max_depth;
-    for (i = nodes.length; --i >= 0; ) {
-      var node = nodes[i];
-      if (!node.px && !node.fixed) {
-        var kkx = kx * max_width / level_widths[node.depth];
-        node.y *= ky;
-        //node.y += 10 + ky / 2;
-        node.x *= kkx;
-        node.x += 10 + kkx / 2;
-        node.x = width / 2;
+    if (0) {
+      // now correct/balance the x positions:
+      max_width = 1;
+      for (i = level_widths.length; --i > 0; ) {
+        max_width = Math.max(max_width, level_widths[i]);
+      }
+      kx = (width - 20) / max_width;
+      ky = (height - 20) / max_depth;
+      for (i = nodes.length; --i >= 0; ) {
+        var node = nodes[i];
+        if (!node.px && !node.fixed) {
+          var kkx = kx * max_width / level_widths[node.depth];
+          node.y *= ky;
+          //node.y += 10 + ky / 2;
+          node.x *= kkx;
+          node.x += 10 + kkx / 2;
+          node.x = width / 2;
 
-        node.qx = node.px = node.x;
-        node.qy = node.py = node.y;
+          node.qx = node.px = node.x;
+          node.qy = node.py = node.y;
+        }
       }
     }
-  }
 
-  return nodes;
-}
+    return nodes;
+    }
   };
 
   //After grabbing relatives from the DB, create a bubbles array based on length of relatives array
