01b8ce20e99abbd2d739e38dc65e4ba73a6cc7eb
Gar Lee <gardarr@gmail.com>
2016-03-29 14:42:39 -0700

    Implementing footer

--- static/app/directives/dst_footer.js
+++ static/app/directives/dst_footer.js
@@ -1,30 +1,3 @@
-angular.module('genome.directive', [])
+angular.module('genome.footer', [])
 
-.directive('dstFooter', function() {
-
-  return {
-    controller: function($scope, $cookies, $rootScope, $location, SelfFactory) {
-      $scope.user_first_name = $cookies.user_first_name;
-      $scope.expand = false;
-
-      $scope.onpoolpage = $location.$$path === '/pool' ? true : false;
-      $scope.onselfpage = $location.$$path === '/self' ? true : false;
-
-      $scope.getRelatives = function () {
-        $scope.onpoolpage = true;
-        $scope.onselfpage = false;
-        $rootScope.removeHelix();
-        $location.path('/pool');
-      };
-
-      $scope.getSelf = function () {
-        $scope.onselfpage = true;
-        $scope.onpoolpage = false;
-        $rootScope.removeMap();
-        $location.path('/self');
-      };
-    },
-    templateUrl: '../static/app/directives/dst_footer.html'
-  };
-
-});
+.directive('dst-footer')
