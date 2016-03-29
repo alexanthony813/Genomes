d8600403e59c64dac9deba43871b78dc38ce0ccd
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-29 12:15:23 -0700

    Routing to back and forth from tree page now working

--- static/app/app.js
+++ static/app/app.js
@@ -38,7 +38,8 @@ var app = angular.module('genome', [
     })
     .when('/tree', {
       templateUrl: '/static/app/tree/tree.html',
-      controller: 'TreeController'
+      controller: 'TreeController',
+      authenticate: true
     })
     .otherwise({
       redirectTo : '/signin'
