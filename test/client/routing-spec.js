65258fa42fb6ec453a77b4e1bb94266b659458cb
Chris Bassano <christo4b@gmail.com>
2016-03-30 16:56:13 -0700

    routing-spec tests pool and self routes

--- test/client/routing-spec.js
+++ test/client/routing-spec.js
@@ -1,6 +1,4 @@
-// 'use strict';
-
-describe('Routing', function () {
+ describe('Routing', function () {
   var $route;
   var AuthController;
 
@@ -8,11 +6,7 @@ describe('Routing', function () {
     module('genome');
 
     inject(function ($injector) {
-      // $routeProvider = $injector.get('$routeProvider');
-      // $location = $injector.get('$location');
-      // $routeParams = $injector.get('$routeParams');
       $route = $injector.get('$route');
-      // AuthController = $injector.get('AuthController')
     });
   });
 
@@ -22,6 +16,18 @@ describe('Routing', function () {
     expect($route.routes['/'].redirectTo).to.equal('/self');
     expect($route.routes['/signin'].controller).to.equal('AuthController');
     expect($route.routes['/signin'].templateUrl).to.equal('/');
+  });
+
+  it('Should have /pool route, template, and controller', function () {
+    expect($route.routes['/pool']).to.be.defined;
+    expect($route.routes['/pool'].controller).to.equal('PoolController');
+    expect($route.routes['/pool'].templateUrl).to.equal('/static/app/pool/pool.html');
+  });
+
+  it('Should have /self route, template, and controller', function () {
+    expect($route.routes['/self']).to.be.defined;
+    expect($route.routes['/self'].controller).to.equal('SelfController');
+    expect($route.routes['/self'].templateUrl).to.equal('/static/app/self/self.html');
   });
 
 });
