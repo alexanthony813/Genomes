b63d1d940ea8778bd84d55d71976580cdfb73857
Alex Anthony <alexanderanthony813@gmail.com>
2016-03-30 15:53:05 -0700

    Took out unnecessary it blocks

--- test/client/routing-spec.js
+++ test/client/routing-spec.js
@@ -16,26 +16,10 @@ describe('Routing', function () {
 
 
   it('Should have /signin route, template, and controller', function () {
-    // expect($routeProvider.routes['/']).to.be.defined;
-    // expect($route.routes['/'].controller).to.equal('AuthController');
-    // expect($route.routes['/'].templateUrl).to.equal('/');
-    // expect($routeProvider).to.exist;
-    expect($route).to.exist;
+    expect($route.routes['/']).to.be.defined;
+    expect($route.routes['/'].controller).to.equal('AuthController');
+    expect($route.routes['/'].templateUrl).to.equal('/');
+    // expect($route).to.exist;
   });
   
 });
-
-
-
-
-  // it('Should have /pool route, template, and controller', function () {
-  //   // expect($route.routes['/pool']).to.be.defined;
-  //   // expect($route.routes['/pool'].controller).to.equal('PoolController');
-  //   // expect($route.routes['/pool'].templateUrl).to.equal('/static/app/pool/pool.html');
-  // });
-
-  // it('Should have /self route, template, and controller', function () {
-  //   // expect($route.routes['/self']).to.be.defined;
-  //   // expect($route.routes['/self'].controller).to.equal('SelfController');
-  //   // expect($route.routes['/self'].templateUrl).to.equal('/static/app/self/self.html');
-  // });
