a110bffc0755524eaa0917a7f39e2ffe202e02f1
Alex Anthony <alexanderanthony813@gmail.com>
2016-04-01 13:31:46 -0700

    Took out comments

--- static/app/tree/tree.js
+++ static/app/tree/tree.js
@@ -291,34 +291,6 @@ angular.module('genome.tree', ['genome.treeService'])
       node = node.data(nodes, function(d) { return d.id; });
 
       node.exit().remove();
-      
-       // var drag = d3.behavior.drag()
-
-    // var node_drag = d3.behavior.drag()
-        // .on("dragstart", dragstart)
-        // .on("drag", dragmove)
-        // .on("dragend", dragend);
-
-    // function dragstart(d, i) {
-    //   console.log('start')
-    //     force.stop() // stops the force auto positioning before you start dragging
-    // }
-
-    // function dragmove(d, i) {
-    //   console.log('move')
-    //     d.px += d3.event.dx;
-    //     d.py += d3.event.dy;
-    //     d.x += d3.event.dx;
-    //     d.y += d3.event.dy; 
-    //     tick(); // this is the key to make it work together with updating both px,py,x,y on d !
-    // }
-
-    // function dragend(d, i) {
-    //   console.log('end')
-    //     d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
-    //     tick();
-    //     force.resume();
-    // }
 
       var nodeEnter = node.enter().append('g')
           .attr('padding', 50)
@@ -328,16 +300,16 @@ angular.module('genome.tree', ['genome.treeService'])
               if(node.relationship === 'paternal_side' || node.relationship === 'me' || node.relationship === 'maternal_side'){
                 node.fixed = false;
               }
-            })
+            });
           })
           .on('mouseup', function(){
             nodes.forEach(function(node){
               if(node.relationship === 'paternal_side' || node.relationship === 'me' || node.relationship === 'maternal_side'){
                 node.fixed = true;
               }
-            })
+            });
           })
-          .call(force.drag)
+          .call(force.drag);
 
       nodeEnter.append('circle')
           .attr('fill', function(d){
@@ -441,12 +413,6 @@ angular.module('genome.tree', ['genome.treeService'])
        .attr("y1", function(d) { return d.source.y; })
        .attr("x2", function(d) { return d.target.x; })
        .attr("y2", function(d) { return d.target.y; });
-    
-    // console.log('links', links)
-    // links.forEach(function(d, i) {
-    //   d.source.y -= k;
-    //   d.target.y += k;
-    // });
 
     node.attr('transform', function(d) {
       var nodeX = Math.max(radius, Math.min(svgWidth - radius, d.x));
